3f0f9ccadcb58b2ea17df6df7f2e3aa2d73606cb
Robin Ward <robin.ward@gmail.com>
2014-05-14 15:24:26 -0400

    First version

---
+++ README.md
@@ -0,0 +1,4 @@
+### folksy-store-name
+
+A plugin to show a store name on the Folksy forums. Uses pre-existing user custom fields.
+
---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: folksy-store-name
+# about: Show a link to a store in posts on the folksy forum
+# version: 0.1
+# authors: Robin Ward
+
+register_asset('javascripts/folksy-store-name.js')
+register_asset('stylesheets/folksy-store-name.css')
